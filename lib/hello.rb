5fffaa08867856f61bba0e16d58221aa255c0d35
Jack Goode <jack.w.goode@icloud.com>
2020-06-07 13:27:49 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -4,11 +4,13 @@ def hello_t(array)
   
   while i < array.length
     yield array[i]
-    i += 1
+    i = i + 1
   end
+  
   array
 else
   puts "Hey! No block was given!"
+  end
 end
 
 # call your method here!
